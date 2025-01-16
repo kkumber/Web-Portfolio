1f83ada09ce063edc8013d4d99832cbd8aec5eb0
kkumber <angelbertreyes28@gmail.com>
2025-01-16 21:39:19 +0800

    no

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import GlassEffectContainer from "../Components/GlassEffectContainer";
 const Home = () => {
     return (
     // Main Grid
-    <div className="grid grid-cols-3 grid-rows-5 gap-2">
+    <div className="grid grid-cols-3 grid-rows-5 gap-2 text-white">
         <div className="col-start-1 col-end-3 row-start-1 row-end-2 ">Div 1</div>
         <div className="col-start-1 col-end-2 row-start-2 row-end-4 ">Div 2</div>
         <div className="col-start-1 col-end-2 row-start-4 row-end-6 ">Div 3</div>
