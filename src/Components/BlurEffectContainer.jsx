d5cb3caa4da3a22488809e09d75776653e74ca17
kkumber <angelbertreyes28@gmail.com>
2025-01-16 03:36:20 -0800

    s

--- src/Components/BlurEffectContainer.jsx
+++ src/Components/BlurEffectContainer.jsx
@@ -1,7 +1,7 @@
-const BlurEffectContainer = ({title, content}) => {
+const BlurEffectContainer = ({title, content, customClassName}) => {
     return ( 
         <div className="relative bg-ContainerLinearGray shadow-xl bg-opacity-70 backdrop-blur-xl rounded-sm p-4">
-            <p><b>{title}</b></p>
+            <p className={customClassName}><b>{title}</b></p>
             <p>{content}</p>
         </div>
      );
--- src/Components/GlassEffectContainer.jsx
+++ src/Components/GlassEffectContainer.jsx
@@ -1,8 +1,8 @@
-const GlassEffectContainer = ({title, content}) => {
+const GlassEffectContainer = ({title, content, customClassName}) => {
     return ( 
         <div className="bg-gradient-to-bl from-ContainerLinearDark from-30% via-backgroundLinearLightGray shadow-md
         backdrop-blur-2xl border-borderColor p-4 text-white rounded-sm">
-            <p><b>{title} </b></p>
+            <p className={customClassName}><b>{title} </b></p>
             <p>{content} </p>
         </div>
      );
