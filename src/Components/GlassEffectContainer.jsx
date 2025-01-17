7ec239332898c0b38a0603ea17552a837f5ea7aa
kkumber <angelbertreyes28@gmail.com>
2025-01-17 16:42:56 +0800

    contact page is now done

--- src/Components/GlassEffectContainer.jsx
+++ src/Components/GlassEffectContainer.jsx
@@ -1,9 +1,10 @@
-const GlassEffectContainer = ({title, content, customClassName}) => {
+const GlassEffectContainer = ({title, content, customClassName, customLayout}) => {
     return ( 
         <div className="bg-gradient-to-bl from-ContainerLinearDark from-30% via-backgroundLinearLightGray shadow-md
         backdrop-blur-2xl border-borderColor p-4 text-white rounded-sm w-full h-full">
             <p className={customClassName}><b>{title} </b></p>
             <p>{content} </p>
+            {customLayout}
         </div>
      );
 }
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -4,8 +4,6 @@ import ProjectContainer from "../Components/ProjectContainer";
 const Projects = () => {
     const projects = [
         {image: '/images/projects/diyeats.png', title: 'DIYeats', description: 'A modern, interactive recipe finder app that empowers users to discover, explore, and save recipes effortlessly. Diyeats integrates advanced search functionality, curated content, and nutritional insights to enhance the cooking experience.', tech: ['React', 'Typescript', 'TailwindCSS'], github: 'https://github.com/kkumber/diyeats', demo: 'https://recipe-finder-app-roan.vercel.app'},
-        {image: '/images/projects/diyeats.png', title: 'DIYeats', description: 'A modern, interactive recipe finder app that empowers users to discover, explore, and save recipes effortlessly. Diyeats integrates advanced search functionality, curated content, and nutritional insights to enhance the cooking experience.', tech: ['React', 'Typescript', 'TailwindCSS'], github: 'https://github.com/kkumber/diyeats', demo: 'https://recipe-finder-app-roan.vercel.app'},
-
         // {image: '', title: '', description: '', tech: [], github: '', demo: 'demo'},
         // {image: '', title: '', description: '', tech: [], github: '', demo: 'demo'},
         // {image: '', title: '', description: '', tech: [], github: '', demo: 'demo'},
